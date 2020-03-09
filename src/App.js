b9a3e6fedf7b3f7fa4a3400055ee3e60f11843fd
jane1002 <jane.fan1002@gmail.coom>
2020-03-09 07:45:54 -0500

    Start project.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyUploader from './Components/Upload/Upload';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyUploader></MyUploader>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Upload/Upload.js
@@ -0,0 +1,26 @@
+import 'react-dropzone-uploader/dist/styles.css';
+import Dropzone from 'react-dropzone-uploader';
+import React from 'react';
+
+const MyUploader = () => {
+  // specify upload params and url for your files
+  const getUploadParams = ({ meta }) => { return { url: 'http://localhost:3000/upload' } }
+
+  // called every time a file's `status` changes
+  const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
+
+  // receives array of files that are done uploading when submit button is clicked
+  const handleSubmit = (files, allFiles) => {
+    console.log(files.map(f => f.meta));
+    allFiles.forEach(f => f.remove())
+  };
+  return (
+      <Dropzone
+          getUploadParams={getUploadParams}
+          onChangeStatus={handleChangeStatus}
+          onSubmit={handleSubmit}
+      />
+  )
+};
+
+export default MyUploader;
